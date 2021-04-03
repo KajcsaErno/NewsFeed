c7514cfaa50be14d06dcde44985ce99e94cab3dd
Erno Kajcsa <kajcsaerno@gmail.com>
2021-04-03 16:42:47 +0200

    Added README.md

--- app/build/generated/source/buildConfig/debug/com/example/android/newsfeed/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/android/newsfeed/BuildConfig.java
@@ -7,9 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.example.android.newsfeed";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
-  // Fields from build type: debug
+  // Field from build type: debug
   public static final String GUARDIAN_API_KEY = "1e21eb6d-5bc1-4777-a08c-3311e0ce8b9d";
 }
